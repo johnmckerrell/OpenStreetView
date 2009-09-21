3bd71ec4032dd5ea7bc66c2b14272eaeaae47235
John McKerrell <john@mckerrell.net>
2009-09-21 22:02:36 +0100

    Locking tables while processing.

--- script/tools/processor.rb
+++ script/tools/processor.rb
@@ -3,13 +3,13 @@ require File.dirname(__FILE__) + "/../../config/environment"
 
 while true do
 
-  #ActiveRecord::Base.connection.execute("LOCK TABLES photos WRITE")
+  ActiveRecord::Base.connection.execute("LOCK TABLES photos WRITE")
   p = Photo.find(:first, :conditions => { :status => 'pending' } )
   if p
     p.status = 'processing'
     p.save
   end
-  #ActiveRecord::Base.connection.execute("UNLOCK TABLES")
+  ActiveRecord::Base.connection.execute("UNLOCK TABLES")
   if p
     p.process
     puts "Sleeping for 5 seconds"
