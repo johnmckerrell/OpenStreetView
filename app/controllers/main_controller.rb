f4b7666cdecf5eaf0d8d913a8769ca2416c37f11
John McKerrell <john@mckerrell.net>
2009-10-31 12:27:57 +0000

    Fix for footer on moderation page.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,7 @@ class MainController < ApplicationController
   def index
     @javascripts = [ 'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js', 'jquery/jquery.json-2.2.min', 'jquery/boxer.js', '/openlayers/OpenLayers', '/openlayers/OpenStreetMap', 'OSV', 'OSVMain', 'OSVPhoto' ]
     @unlocated = Photo.find_unlocated
+    @pageid = 'homepage'
   end
 
   def licenses
